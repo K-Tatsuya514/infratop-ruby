500b6be8c48c98d4b314b2b5fd33b818b0a7277d
K-Tatsuya514 <kobatatsu0514@gmail.com>
2020-07-22 15:25:00 +0900

    [Add]dir2-6

---
+++ 2-6/lesson5.rb
@@ -0,0 +1,16 @@
+puts "Rubyを学習"
+name = "小林さん"
+puts "Hello" + name
+puts "こんにちは#{name}"
+
+names = ["Perl","Ruby","Python","Swift"]
+puts names[1]
+
+tall = {"太郎"=>185, "二郎"=>170, "花子"=>150}
+puts tall["太郎"]
+
+tall2 = {:太郎=>183,:次郎=>170,:花子=>140}
+puts tall2[:次郎]
+
+subjects = [:国語, :算数, :理科, :社会]
+puts subjects[2]
